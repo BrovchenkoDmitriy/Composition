d54122ed8d397e9d0e7b4a9c3c63285776c49c4d
Morak <d.a.brovchenko@gmail.com>
2022-04-13 16:50:16 +0900

    navigation

--- app/src/main/java/com/example/composition/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/example/composition/presentation/WelcomeFragment.kt
@@ -1,13 +1,12 @@
 package com.example.composition.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.composition.R
 import com.example.composition.databinding.FragmentWelcomeBinding
-import java.lang.RuntimeException
 
 
 class WelcomeFragment : Fragment() {
@@ -29,12 +28,20 @@ class WelcomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.buttonUnderstand.setOnClickListener {
-
+            launchChooseLevelFragment()
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun launchChooseLevelFragment() {
+        requireActivity().supportFragmentManager.beginTransaction().replace(
+            R.id.main_container,
+            ChooseLevelFragment.newInstance()
+        ).addToBackStack(null).
+        commit()
     }
 }
